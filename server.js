0b99a54123e3a2b6d980392a6882e301eeab8de9
Pavel-V <pavelvarigin@mail.ru>
2018-04-12 16:13:34 +0700

    Full code from article about node + express + mongo

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const db             = require('./config/db');
+
 const app            = express();
 
-require('./app/routes')(app, {});
-
 const port = 8000;
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, (err, database) => {
+
+	const db = database.db('node-mongo-app');	
+
+	if (err) return console.log(err)
+	require('./app/routes')(app, db);
+	app.listen(port, () => {
+	console.log('We are live on ' + port);
+});     
+
+})
+
+
+// node-mongo-app
+
+// notes
+
+/* MongoClient.connect(db.url,(err,database) =>{ 
+  const myAwesomeDB = database.db('myDatabaseNameAsAString')
+  myAwesomeDB.collection('theCollectionIwantToAccess')
+} */
